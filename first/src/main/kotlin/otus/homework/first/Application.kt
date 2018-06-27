0ad04e8a8ee98ad2c33ea872008f81a86af089e9
vladislav.fedotov <java.lang@yande.xru>
2018-06-27 15:31:45 +0300

    First homework

---
+++ first/src/main/kotlin/otus/homework/first/Application.kt
@@ -0,0 +1,20 @@
+package otus.homework.first
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import otus.homework.first.service.QuizzService
+
+@SpringBootApplication
+class Application {
+    @Bean
+    fun run(quizzService: QuizzService) = CommandLineRunner {
+        quizzService.startQuizz()
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
+
